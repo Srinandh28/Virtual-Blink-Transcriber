48dc5090461a237149ffb7084d12f4f916639569
Srinandh <130382295+Srinandh28@users.noreply.github.com>
2024-02-13 17:47:39 +0530

    Update sendSMS.py

--- VBT/sendSMS.py
+++ VBT/sendSMS.py
@@ -7,13 +7,13 @@ def send(msg):
     client=Client(account_sid, auth_token)
     if msg:
         client.messages.create(
-        to="+919791172932", #fill in appropriately
-        from_="+919842903913", #fill in appropriately
+        to="", #fill in appropriately
+        from_="", #fill in appropriately
         body=msg)
     else:
         client.messages.create(
-        to="+919791172932", #fill in appropriately
-        from_="+919842903913",#fill in appropriately
+        to="", #fill in appropriately
+        from_="",#fill in appropriately
         body="Default Message")#fill in appropriately
         
     
